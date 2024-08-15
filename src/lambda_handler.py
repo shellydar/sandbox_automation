9431cfe22ef5eef67ce993a5fab04ce18be183fa
Shelly Dar <shelly.dar@gmail.com>
2024-08-15 17:23:53 +0100

    bug fixes

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -6,7 +6,7 @@ org_client = boto3.client('organizations')
 
 def get_tag_name():
     ssm_client = boto3.client('ssm')
-    tag_name = ssm_client.get_parameter(Name="Control_Tower/account_expiration")['Parameter']['Value']
+    tag_name = ssm_client.get_parameter(Name="/Control_Tower/account_expiration")['Parameter']['Value']
     return tag_name
 
 def get_accounts(ou_id):
@@ -19,12 +19,19 @@ def get_accounts(ou_id):
 
 #get OU ID for Sandbox OU
 def get_ou_id(root_id, ou_name):
+    print(f"getOU ID for Root ID: {root_id} and ou_name {ou_name}")
     response = org_client.list_organizational_units_for_parent(ParentId=root_id)
-    while 'NextToken' in response:
-        response = org_client.list_organizational_units_for_parent(ParentId=root_id, NextToken=response['NextToken'])
-        for ou in response['OrganizationalUnits']:
-            if ou['Name'] == ou_name:
-                return ou['Id']
+    if "NextToken" in response:
+        while "NextToken" in response:
+            response = org_client.list_organizational_units_for_parent(ParentId=root_id, NextToken=response["NextToken"])
+            for ou in response["OrganizationalUnits"]:
+                print(f"Response: {response}")
+                if ou["Name"].lower() == ou_name.lower:
+                    return ou["Id"]
+    else:
+        for ou in response["OrganizationalUnits"]:
+            if ou["Name"].lower() == ou_name.lower():
+                return ou["Id"]
 
 def get_account_tags(account_id):
     response = org_client.list_tags_for_resource(ResourceId=account_id)
@@ -42,32 +49,44 @@ def check_expiration(tag_value):
     else:
         return False
 
+#get account expiration date from tag
+def get_account_expiration_date(account_id, tag_name):
+    response = org_client.list_tags_for_resource(ResourceId=account_id)
+    for tag in response["Tags"]:
+        if tag["Key"] == tag_name:
+            return tag["Value"]
+    return None
+
 # list accounts in AWS OU called "Sandbox"
 
 root_id = org_client.list_roots()['Roots'][0]['Id']
-ou_id = get_ou_id(root_id, "Sandbox")
+print(f"Root ID: {root_id}")
+ou_id = get_ou_id(root_id=root_id, ou_name="Sandbox")
+print(f"Sandbox OU ID: {ou_id}")
+if ou_id is None:
+    print("Sandbox OU not found.")
+    exit(1)
 accounts = get_accounts(ou_id)
+if len(accounts) == 0:
+    print("No accounts found in Sandbox OU.")
+    exit(0)
 tag_name=get_tag_name()
 accounts_to_expire = [] #list to store accounts to expire = []
-
+accounts_with_no_tag = []
+accounts_to_keep = []
 for Id in accounts:
-    account_tags = get_account_tags(Id)
-    for tag in account_tags:
-        if tag['Key'] == tag_name:
-            if check_expiration(tag['Value']):
-                accounts_to_expire.append(Id)
-                print(f"Account {Id} needs to be expired.")
-                break
-            
-            else:
-                print(f"Account {Id} does not need to be expired.")
-                break
-            break
-        else:
-            print(f"Account {Id} does not have the {tag_name} tag.")
-            break
-        break
-    break
+    expiration_date=get_account_expiration_date(Id=Id, tah_name=tag_name)
+    if expiration_date is None:
+        accounts_with_no_tag.append(Id)
+        print(f"Account {Id} does not have the {tag_name} tag.")
+        continue
+    elif expiration_date.todate() < datetime.now().date():
+        accounts_to_expire.append(Id)
+        print(f"Account {Id} has expiration date set to {expiration_date}. It needs to be expired.")
+    else:
+        accounts_to_keep.append(Id)
+        print(f"Account {Id} does not need to be expired. It's expiration date is:{expiration_date} ")
+
 print(f"Accounts to expire: {accounts_to_expire}")
 print(f"Number of accounts to expire: {len(accounts_to_expire)}")
 
